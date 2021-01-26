2c09e4577a809b1d5a88e32d789cfb8b0ac19395
ThayanaRMachado <thayanarm@hotmail.com>
2021-01-26 16:39:42 -0300

    Implementando segurança com OAuth 2 e Password Flow

---
+++ src/main/java/br/com/algamoneyapifinal/config/AuthorizationServerConfig.java
@@ -0,0 +1,44 @@
+package br.com.algamoneyapifinal.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration //Classe de configuração.
+@EnableAuthorizationServer //Habilita a segurança. Essa anotação já vem com o @Configuration, portanto não é obrigatório colocá-la no código Só se quiser.
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{ //com o WebSecurityConfigurerAdapter, ganha-se alguns métodos p/ customizar a configuração.
+	
+	@Autowired
+	private AuthenticationManager authenticationManager; //Vai gerenciar a autenticação pegando o usuário e a senha qque estão cadastrados na ResourceServerConfig (.withUser("admin").password("admin")).
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception { //Configura a aplicação, que é o Client, que é quem o usuário está usando. Eu vou autorizar esse Client usar o Authorization Server. 
+		clients.inMemory() //O Client pode ficar em memória ou em jdbc, no banco de dados.
+		.withClient("angular") //qual é o nome do Client
+		.secret("@ngul@r0") //qual é a senha do Client
+		.scopes("read", "write") //uma String define o escopo do cliente. Com ele, pode-se evitar o acesso do cliente angular.Podem ser definidos escopos != p/ clientes !=. O tratamento dessas Strings é feito nos métodos.
+		.authorizedGrantTypes("password") //Tipos de concessão autorizados-O usuário digita usuário e senha na tela do Angular e o Angular no JavaScript recebe esse usuário e senha e envia p/ pegar o Access Token.O Angular tem acesso ao usuário e à senha. Isso só é feito qdo. se tem extrema confiança na aplicação. Como a aplicação Angular será feita no curso, essa é a configuração ideal p/ utilizar.
+		.accessTokenValiditySeconds(1800); //Qtos. segundos o token ficará ativo. Nesse caso, será 30 minutos.
+		super.configure(clients);
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception { //Recebe o AuthorizationServerEndpointsConfigurer
+		endpoints.tokenStore(tokenStore()) //O Token será armazendo em um Token Store. Porque a aplicação Angular vai buscar o Token e vai mandá-lo de volta p/ conseguir acessar a API(/lancamentos).O Token tem que estar armazenado em algum lugar, p/ saber se ele é válido ou inválido.
+		.authenticationManager(authenticationManager); //Para validar o usuário e a senha e ver se está tudo certo.
+	}
+
+	@Bean
+	public TokenStore tokenStore() { //Onde o Token será armazenado.
+		return new InMemoryTokenStore(); //O armazenamento ocorrerá em memória. Poderia ser feito no banco de dados também.
+	}
+
+
+}
--- src/main/java/br/com/algamoneyapifinal/config/ResourceServerConfig.java
+++ src/main/java/br/com/algamoneyapifinal/config/ResourceServerConfig.java
@@ -1,30 +1,38 @@
 package br.com.algamoneyapifinal.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @Configuration //Classe de configuração.
 @EnableWebSecurity //Habilita a segurança. Essa anotação já vem com o @Configuration, portanto não é obrigatório colocá-la no código Só se quiser.
-public class SecurityConfig extends WebSecurityConfigurerAdapter{ //com o WebSecurityConfigurerAdapter, ganha-se alguns métodos p/ customizar a configuração.
+@EnableResourceServer //Habilita o Resource Server.
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{ //ResourceServerConfig vai exercer a função do Resource Server.Com o ResourceServerConfigurerAdapter, ganha-se alguns métodos p/ customizar a configuração.
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Autowired //Injeta o AuthenticationManagerBuilder
+	public void configure(AuthenticationManagerBuilder auth) throws Exception { //Construtor de gerenciador de autenticação
 		auth.inMemoryAuthentication() //Onde o usuário e a senha serão validados. Pode-se buscar no banco de dados, mas aqui ficará em memória.
-		.withUser("admin").password("{noop}admin").roles("ROLE"); //Na autenticação em memória passa-se o usuário, a senha, (para uma autenticação) e uma permissão, (para uma autorização), que esse usuário poderia ter.A permissão não será utilizada agora.
+		.withUser("admin").password("admin").roles("ROLE"); //Na autenticação em memória passa-se o usuário, a senha, (para uma autenticação) e uma permissão, (para uma autorização), que esse usuário poderia ter.A permissão não será utilizada agora.
 	}
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception { //Configura as autorizações das requisições
+	public void configure(HttpSecurity http) throws Exception { //Configura as autorizações das requisições
 		http.authorizeRequests() //Autoriza requisições
 		.antMatchers("/categorias").permitAll() //Para /categorias, qualquer um pod acessar.
 		.anyRequest().authenticated() //Para qualquer requisição é preciso estar autenticado (usuário e senha validados).
 		.and()
-		.httpBasic().and() //Tipo de autenticação utilizada. httpBasic()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and() //Desabilita a criação de sessão no servidor. A API REST não terá esado de nada.
 		.csrf().disable(); //Desabilita o csrf para que não se possa ser feito um Java Script injection dentro do serviço web.Como na aplicação não tem parte web, ele pode ser desabilitado.
+	}
+	
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception{
+		resources.stateless(true); //Para deixar o servidor sem estado. Não vai armazenar nada no resource.
 	}
 }
